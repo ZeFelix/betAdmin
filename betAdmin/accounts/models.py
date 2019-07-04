ac4618ca6873e926023ec618cf1b9e68f8584423
Félix <jophev3@hotmail.com>
2019-07-03 22:33:42 -0300

    created model query

--- betAdmin/accounts/models.py
+++ betAdmin/accounts/models.py
@@ -5,9 +5,17 @@ from django.dispatch import receiver
 
 
 class Profile(User):
+
     phone = models.CharField(max_length=20, blank=True, null=True)
     country = models.CharField(max_length=20, blank=True, null=True)
     city = models.CharField(max_length=20, blank=True, null=True)
     zip_code = models.CharField(max_length=20, blank=True, null=True)
     street_address = models.CharField(max_length=50, blank=True, null=True)    
     email_confirmed = models.BooleanField(default=False)
+
+
+class Query(models.Model):
+
+    name = models.CharField(max_length=50)
+    query = models.CharField(max_length=250)
+    profile = models.ForeignKey(Profile, related_name='query', on_delete=models.CASCADE)
