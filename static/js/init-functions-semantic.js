827d208af0666a337cf2b4ca24fb2edcd0a69c5f
Félix <jophev3@hotmail.com>
2019-07-10 20:57:59 -0300

    moved files.js from startup to file init-functions-semantic.js

---
+++ static/js/init-functions-semantic.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    /**function to start close messages content*/
+    $('.message .close')
+        .on('click', function() {
+            $(this)
+                .closest('.message')
+                .transition('fade');
+        });
+    /**function to start tabs */
+    $('.menu .item').tab();
+});
