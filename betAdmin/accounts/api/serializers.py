0c82310ee78e98f0714d57fdf9abf608269ea370
Félix <jophev3@hotmail.com>
2019-09-16 20:41:12 -0300

    api register simple user ok

--- betAdmin/accounts/api/serializers.py
+++ betAdmin/accounts/api/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from betAdmin.accounts.models import Profile
 
 
 class SimpleProfileSerializer(ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=Profile.objects.all())])
+
     class Meta:
         model = Profile
-        fields = ['id', 'username', 'email', 'first_name', 'email_confirmed']
+        fields = ['id', 'username', 'email', 'first_name', 'email_confirmed', 'password']
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
